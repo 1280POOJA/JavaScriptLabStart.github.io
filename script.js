1655ef153bd00646feec5f6576c542aa341fc795
1280POOJA <103152174+1280POOJA@users.noreply.github.com>
2022-04-18 12:19:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    // get the operands value
+
+
+
+    // parse to integer. You can use: parseInt(input, 10);
+
+    // perform operation
+
+    // creating the text output
+
+    // (if you want to check) print out in console
+
+    // change the text area
+
